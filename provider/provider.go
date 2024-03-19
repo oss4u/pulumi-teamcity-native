2024b2af63ec2d55906fa88d18313f57ca411fb5
Marc Ende <me@e-beyond.de>
2024-03-19 14:43:48 +0000

    fix: empty

--- provider/provider.go
+++ provider/provider.go
@@ -38,9 +38,9 @@ func Provider() p.Provider {
 			Publisher:   "Oss4u",
 			Homepage:    "https://github.com/oss4u/",
 			LanguageMap: map[string]any{
-				"python": map[string]any{
-					"Download-URL": "https://github.com/oss4u/pulumi-teamcity-native?VERSION",
-				},
+				// "python": map[string]any{
+				// 	"download_url": "https://github.com/oss4u/pulumi-teamcity-native?VERSION",
+				// },
 				"nodejs": map[string]any{
 					"packageName": "@oss4u/teamcity",
 				},
